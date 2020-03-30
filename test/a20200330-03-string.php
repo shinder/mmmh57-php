b631266949928efc894bf0e1b9a834dac215f371
Shinder Lin <shinder.lin@gmail.com>
2020-03-30 14:57:08 +0800

    String

---
+++ test/a20200330-03-string.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 'Shinder';
+
+echo 'My name is $a<br>'; // 單引號會呈現原本的內容
+echo "My name is $a<br>"; // 雙引號會把變數用變數值取代
+
+echo "My name is {$a}123<br>"; //
+echo "My name is ${a}123<br>"; //
+echo "My name is $a123<br>"; // Notice
+
+echo "<h2>
+Hello
+
+</h2>";
+
+// Notice
+// Warning
+// Error
+
+
