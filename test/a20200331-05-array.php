862505483897ec9a016b7575ed277b9ba017310d
txs <txshon@gmail.com>
2020-03-31 14:20:55 +0800

    array

---
+++ test/a20200331-05-array.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<pre>
+<?php
+
+$ar1 = array(2,3,4,5, 'hello');
+$ar2 = [2,3,4,5];
+
+$ar1[] = '您好'; // array push
+
+print_r($ar1);
+
+var_dump($ar1);
+
+
+?>
+    </pre>
+</body>
+</html>
