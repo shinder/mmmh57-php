61c3144ccc66399725bc57535cf8f8cc8cef2e8e
txs <txshon@gmail.com>
2020-03-31 16:31:17 +0800

    bootstrap-test: 拆檔案

---
+++ test/parts/html-head.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <script src="../js/jquery-3.4.1.js"></script>
+    <script src="../bootstrap/js/bootstrap.bundle.js"></script>
+</head>
+<body>
