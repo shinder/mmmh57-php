91c6b63000c1ef292c90cae93a9d9e2a62e0496c
txs <txshon@gmail.com>
2020-03-31 13:49:24 +0800

    nest for loop 2

--- test/a20200331-04-nest-for.php
+++ test/a20200331-04-nest-for.php
@@ -8,15 +8,27 @@
     <title>Document</title>
 </head>
 <body>
+
 <table border="1">
     <?php for($i=1; $i<=9; $i++): ?>
     <tr>
         <?php for($k=1; $k<=9; $k++): ?>
-        <td><?= $i*$k ?></td>
+        <td><?= "$i * $k =". $i*$k ?></td>
         <?php endfor; ?>
     </tr>
     <?php endfor; ?>
 </table>
 
+<?php /*
+<table border="1">
+    <?php for($i=1; $i<=9; $i++): ?>
+        <tr>
+            <?php for($k=1; $k<=9; $k++): ?>
+                <td><?= $i*$k ?></td>
+            <?php endfor; ?>
+        </tr>
+    <?php endfor; ?>
+</table>
+*/ ?>
 </body>
 </html>
