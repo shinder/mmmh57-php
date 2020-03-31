e2d2270f0f757a3de4ebca126fbdc9314827ccee
txs <txshon@gmail.com>
2020-03-31 14:38:18 +0800

    foreach/as

---
+++ test/a20200331-06-foreach.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$ar1 = array(2,3,4,5, 'hello', '您好');
+
+foreach($ar1 as  $i){
+    echo "$i <br>";
+}
+
+
+?>
+
+</body>
+</html>
