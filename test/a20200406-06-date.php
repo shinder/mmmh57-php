b66fdf67840f9f5697f68c1247c8c1e3ef2afaad
txs <txshon@gmail.com>
2020-04-06 14:29:25 +0800

    時間函式

--- test/a20200406-06-date.php
+++ test/a20200406-06-date.php
@@ -1,7 +1,18 @@
+<div>
 <?php
 date_default_timezone_set('Asia/Taipei');
 echo date("Y-m-d H:i:s");
+echo '<br>';
 
-
+echo date("Y-m-d H:i:s", time()+50*24*60*60);
+echo '<br>';
+echo date("Y-m-d H:i:s", strtotime('+50 days'));
+echo '<br>';
+echo date("w", time()+50*24*60*60);
+echo '<br>';
+echo date("Y-m-d H:i:s", strtotime('03/06/2019'));
+echo '<br>';
+?>
+</div>
 
 
