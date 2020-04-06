76d1e112af17d189ad34147edb5320408da69e5d
txs <txshon@gmail.com>
2020-04-06 09:25:07 +0800

    html title

--- test/bootstrap-test.php
+++ test/bootstrap-test.php
@@ -1,3 +1,6 @@
+<?php
+//$title = 'Hello';
+?>
 <?php include __DIR__. '/parts/html-head.php';   ?>
 <?php include __DIR__. '/parts/navbar.php';   ?>
 
--- test/parts/html-head.php
+++ test/parts/html-head.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?= isset($title) ? $title.' - ' : '' ?>ABC</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
     <script src="../js/jquery-3.4.1.js"></script>
     <script src="../bootstrap/js/bootstrap.bundle.js"></script>
