022bc99da36f863eb21825adc7f89547bd6d5fc8
txs <txshon@gmail.com>
2020-04-06 10:34:27 +0800

    表單: ajax 準備

---
+++ test/a20200406-03-form-ajax.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="" name="form1" onsubmit="return mySubmit()">
+    <input type="number" id="a"> + <input type="number" id="b"> <button>=</button>
+    <input type="text" id="c" disabled="disabled">
+</form>
+
+
+
+<script src="../js/jquery-3.4.1.js"></script>
+<script>
+    function mySubmit() {
+        return false;
+    }
+</script>
+</body>
+</html>
