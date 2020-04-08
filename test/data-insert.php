5f7aefddc09c697f30f68622c7d601c4b65d4890
shinder <shinder.lin@gmail.com>
2020-04-08 11:27:10 +0800

    新增資料的表單 2

--- test/data-insert.php
+++ test/data-insert.php
@@ -29,17 +29,29 @@
         <div class="col-lg-6">
             <form>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <label for="name">name</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                    <small id="nameHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <label for="email">email</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                    <small id="emailHelp" class="form-text text-muted"></small>
                 </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="form-group">
+                    <label for="mobile">mobile</label>
+                    <input type="text" class="form-control" id="mobile" name="mobile">
+                    <small id="mobileHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="birthday">birthday</label>
+                    <input type="date" class="form-control" id="birthday" name="birthday">
+                    <small id="birthdayHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="address">address</label>
+                    <textarea class="form-control" name="address" id="address" cols="30" rows="3"></textarea>
+                    <small id="addressHelp" class="form-text text-muted"></small>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
