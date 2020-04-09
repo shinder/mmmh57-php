37142a70d487eee88ca4b8d08a8e123be39e0639
shinder <shinder.lin@gmail.com>
2020-04-09 14:57:22 +0800

    新增資料的提示

--- test/data-insert.php
+++ test/data-insert.php
@@ -17,46 +17,59 @@ if(isset($_POST['name']) and isset($_POST['mobile'])) {
         $_POST['address'],
     ]);
 
-
-    echo $stmt->rowCount(); exit;
-
-
-
+    if($stmt->rowCount()==1){
+        $success = true;
+    } else {
+        $success = false;
+    }
 }
-
-
-
 ?>
 <?php include __DIR__ . '/parts/html-head.php'; ?>
 <?php include __DIR__ . '/parts/navbar.php'; ?>
 
 <div class="container">
+    <?php if(isset($success)):  ?>
+        <?php if($success):  ?>
+        <div class="alert alert-success" role="alert">
+            資料新增成功
+        </div>
+        <?php else:  ?>
+        <div class="alert alert-danger" role="alert">
+            資料新增失敗
+        </div>
+        <?php endif;  ?>
+    <?php endif;  ?>
     <div class="row">
         <div class="col-lg-6">
             <form method="post">
                 <div class="form-group">
                     <label for="name">name</label>
-                    <input type="text" class="form-control" id="name" name="name" required>
+                    <input type="text" class="form-control" id="name" name="name" required
+                           value="<?= isset($_POST['name']) ? htmlentities($_POST['name']) : '' ?>">
                     <small id="nameHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
                     <label for="email">email</label>
-                    <input type="email" class="form-control" id="email" name="email">
+                    <input type="email" class="form-control" id="email" name="email"
+                           value="<?= isset($_POST['email']) ? htmlentities($_POST['email']) : '' ?>">
                     <small id="emailHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
                     <label for="mobile">mobile</label>
-                    <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{2}-?\d{3}-?\d{3}" required>
+                    <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{2}-?\d{3}-?\d{3}" required
+                           value="<?= isset($_POST['mobile']) ? htmlentities($_POST['mobile']) : '' ?>">
                     <small id="mobileHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
                     <label for="birthday">birthday</label>
-                    <input type="date" class="form-control" id="birthday" name="birthday">
+                    <input type="date" class="form-control" id="birthday" name="birthday"
+                           value="<?= isset($_POST['birthday']) ? htmlentities($_POST['birthday']) : '' ?>">
                     <small id="birthdayHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
                     <label for="address">address</label>
-                    <textarea class="form-control" name="address" id="address" cols="30" rows="3"></textarea>
+                    <textarea class="form-control" name="address" id="address" cols="30" rows="3"
+                    ><?= isset($_POST['address']) ? htmlentities($_POST['address']) : '' ?></textarea>
                     <small id="addressHelp" class="form-text text-muted"></small>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
