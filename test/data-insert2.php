d9da33829edcfb1a8606d0796b9346b5ce50c664
shinder <shinder.lin@gmail.com>
2020-04-09 15:40:36 +0800

    新增資料的表單檢查 1

--- test/data-insert2.php
+++ test/data-insert2.php
@@ -3,51 +3,46 @@ $page_name = 'data-insert2';
 ?>
 <?php include __DIR__ . '/parts/html-head.php'; ?>
 <?php include __DIR__ . '/parts/navbar.php'; ?>
+    <style>
+        .form-group small.form-text {
+            color: red;
+        }
 
+    </style>
 <div class="container">
-    <?php if(isset($success)):  ?>
-        <?php if($success):  ?>
-        <div class="alert alert-success" role="alert">
-            資料新增成功
+
+        <div id="info-bal" class="alert alert-success" role="alert" style="display: none">
+            123
         </div>
-        <?php else:  ?>
-        <div class="alert alert-danger" role="alert">
-            資料新增失敗
-        </div>
-        <?php endif;  ?>
-    <?php endif;  ?>
+
     <div class="row">
         <div class="col-lg-6">
-            <form method="post">
+            <form method="post" onsubmit="return checkForm()" novalidate>
                 <div class="form-group">
-                    <label for="name">name</label>
-                    <input type="text" class="form-control" id="name" name="name" required
-                           value="<?= isset($_POST['name']) ? htmlentities($_POST['name']) : '' ?>">
-                    <small id="nameHelp" class="form-text text-muted"></small>
+                    <label for="name">* name</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                    <small id="nameHelp" class="form-text"></small>
                 </div>
                 <div class="form-group">
                     <label for="email">email</label>
-                    <input type="email" class="form-control" id="email" name="email"
-                           value="<?= isset($_POST['email']) ? htmlentities($_POST['email']) : '' ?>">
-                    <small id="emailHelp" class="form-text text-muted"></small>
+                    <input type="email" class="form-control" id="email" name="email">
+                    <small id="emailHelp" class="form-text"></small>
                 </div>
                 <div class="form-group">
-                    <label for="mobile">mobile</label>
+                    <label for="mobile">* mobile</label>
                     <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{2}-?\d{3}-?\d{3}" required
-                           value="<?= isset($_POST['mobile']) ? htmlentities($_POST['mobile']) : '' ?>">
-                    <small id="mobileHelp" class="form-text text-muted"></small>
+                           >
+                    <small id="mobileHelp" class="form-text"></small>
                 </div>
                 <div class="form-group">
                     <label for="birthday">birthday</label>
-                    <input type="date" class="form-control" id="birthday" name="birthday"
-                           value="<?= isset($_POST['birthday']) ? htmlentities($_POST['birthday']) : '' ?>">
-                    <small id="birthdayHelp" class="form-text text-muted"></small>
+                    <input type="date" class="form-control" id="birthday" name="birthday">
+                    <small id="birthdayHelp" class="form-text"></small>
                 </div>
                 <div class="form-group">
                     <label for="address">address</label>
-                    <textarea class="form-control" name="address" id="address" cols="30" rows="3"
-                    ><?= isset($_POST['address']) ? htmlentities($_POST['address']) : '' ?></textarea>
-                    <small id="addressHelp" class="form-text text-muted"></small>
+                    <textarea class="form-control" name="address" id="address" cols="30" rows="3"></textarea>
+                    <small id="addressHelp" class="form-text"></small>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
@@ -56,4 +51,20 @@ $page_name = 'data-insert2';
 
 </div>
 <?php include __DIR__ . '/parts/scripts.php'; ?>
+<script>
+    const email_re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    const mobile_re = /^09\d{2}-?\d{3}-?\d{3}$/;
+
+    const $name = $('#name')
+
+    function checkForm(){
+        if($name.val().length < 2){
+            $('#nameHelp').text('請填寫正確的姓名');
+        }
+
+
+
+        return false;
+    }
+</script>
 <?php include __DIR__ . '/parts/html-foot.php'; ?>
