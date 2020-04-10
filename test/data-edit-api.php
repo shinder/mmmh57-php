3d70c24de567454b80cdc08f652c50903e4dcf86
shinder <shinder.lin@gmail.com>
2020-04-10 14:55:03 +0800

    修改資料的 api

--- test/data-edit-api.php
+++ test/data-edit-api.php
@@ -9,54 +9,59 @@ header('Content-Type: application/json');
 
 $output = [
     'success' => false,
-    'error' => '欄位資料不足',
+    'error' => '沒有 sid',
     'code' => 0,
     'postData' => $_POST
 ];
 
-if(isset($_POST['name']) and isset($_POST['mobile'])) {
-    // TODO: 欄位資料檢查
-
-    // 檢查 Email 是否重複
-    $e_sql = "SELECT 1 FROM address_book WHERE email=?";
-    $e_stmt = $pdo->prepare($e_sql);
-    $e_stmt->execute([$_POST['email']]);
-
-    if( $e_stmt->rowCount() ){
-        $output['error'] = 'Email 重複了';
-        echo json_encode($output, JSON_UNESCAPED_UNICODE);
-        exit;
-    }
-
-    // 檢查手機號碼格式
-    $mobile_re = "/^09\d{2}-?\d{3}-?\d{3}$/";
-    if(empty(preg_grep($mobile_re, [ $_POST['mobile']]))){
-        $output['error'] = '手機號碼格式不符';
-        echo json_encode($output, JSON_UNESCAPED_UNICODE);
-        exit;
-    }
-
-
-    $sql = "INSERT INTO `address_book`(
-`name`, `email`, `mobile`, `birthday`, `address`, `created_at`
-) VALUES (?, ?, ?, ?, ?, NOW())";
-
-    $stmt = $pdo->prepare($sql);
-
-    $stmt->execute([
-        $_POST['name'],
-        $_POST['email'],
-        $_POST['mobile'],
-        $_POST['birthday'],
-        $_POST['address'],
-    ]);
-
-    if($stmt->rowCount()==1){
-        $output['success'] = true;
-        $output['error'] = '';
-    } else {
-        $output['error'] = '資料無法新增';
-    }
+$sid = isset($_POST['sid']) ? intval($_POST['sid']) : 0;
+// 如果沒有給 sid, 就回傳訊息然後結束
+if (empty($sid)) {
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    exit;
 }
+
+// TODO: 欄位資料檢查
+
+// 檢查 Email 是否重複
+$e_sql = "SELECT 1 FROM address_book WHERE email=? AND sid<>?";
+$e_stmt = $pdo->prepare($e_sql);
+$e_stmt->execute([$_POST['email'], $sid]);
+
+if( $e_stmt->rowCount() ){
+    $output['error'] = 'Email 重複了';
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+// 檢查手機號碼格式
+$mobile_re = "/^09\d{2}-?\d{3}-?\d{3}$/";
+if(empty(preg_grep($mobile_re, [ $_POST['mobile']]))){
+    $output['error'] = '手機號碼格式不符';
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+
+$sql = "UPDATE `address_book` SET `name`=?,`email`=?,`mobile`=?,`birthday`=?,`address`=? WHERE sid=?";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    $_POST['name'],
+    $_POST['email'],
+    $_POST['mobile'],
+    $_POST['birthday'],
+    $_POST['address'],
+    $sid
+]);
+
+if($stmt->rowCount()==1){
+    $output['success'] = true;
+    $output['error'] = '';
+} else {
+    $output['error'] = '資料沒有修改';
+}
+
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
