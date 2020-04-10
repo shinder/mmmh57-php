c7470ec4bee7051e07e54b391b6ba8ece59a311e
shinder <shinder.lin@gmail.com>
2020-04-10 16:03:16 +0800

    上傳檔案 2

--- test/upload-test01.php
+++ test/upload-test01.php
@@ -1,7 +1,35 @@
 <?php
 $uploads = __DIR__. '/uploads/';
 
-print_r($_FILES['myfiles']);
+$output = [
+    'newname' => '',
+    'name' => '',
+    'error' => ''
+];
 
-// 搬移檔案
-move_uploaded_file($_FILES['myfiles']['tmp_name'], $uploads. $_FILES['myfiles']['name']);
+$ext = '';
+switch($_FILES['myfiles']['type']){
+    case 'image/jpeg':
+        $ext = '.jpg';
+        break;
+    case 'image/png':
+        $ext = '.png';
+}
+
+if(empty($ext)){
+    $output['error'] = '檔案格式不符';
+} else {
+    $filename = md5(uniqid(). $_FILES['myfiles']['name']);
+    $filename .= $ext;
+    $output['newname'] = $filename;
+    $output['name'] = $_FILES['myfiles']['name'];
+    // 搬移檔案
+    move_uploaded_file($_FILES['myfiles']['tmp_name'], $uploads. $filename );
+}
+
+
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
+
+
+
