b7bebbfb8a235dfe24e538cc71df2d6fa78b608e
shinder <shinder.lin@gmail.com>
2020-04-13 13:51:59 +0800

    proj: copy parts

---
+++ proj/parts/html-head.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= isset($title) ? $title : '我的網站' ?></title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../fontawesome/css/all.css">
+
+</head>
+<body>
---
+++ proj/parts/navbar.php
@@ -0,0 +1,36 @@
+<?php
+if(! isset($page_name)){
+    $page_name = '';
+}
+?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item <?= $page_name=='data-list' ? 'active' : '' ?>">
+                    <a class="nav-link" href="data-list.php">資料列表</a>
+                </li>
+                <li class="nav-item <?= $page_name=='data-insert' ? 'active' : '' ?>">
+                    <a class="nav-link" href="data-insert.php">新增資料</a>
+                </li>
+                <li class="nav-item <?= $page_name=='data-insert2' ? 'active' : '' ?>">
+                    <a class="nav-link" href="data-insert2.php">新增資料 2 ajax</a>
+                </li>
+                <li class="nav-item <?= $page_name=='data-list2' ? 'active' : '' ?>">
+                    <a class="nav-link" href="data-list2.php">資料列表 ajax</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<style>
+    #navbarSupportedContent .nav-item.active {
+        background-color: #7abaff;
+    }
+</style>
