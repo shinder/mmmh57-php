2de127f007cdc394590197eb77a48b6cfcbf6673
shinder <shinder.lin@gmail.com>
2020-04-13 14:58:22 +0800

    分類表格資料: php 做出結構

--- proj/categories-api.php
+++ proj/categories-api.php
@@ -2,10 +2,24 @@
 require __DIR__. '/__connect_db.php';
 
 $stmt = $pdo->query("SELECT * FROM categories");
+$rows = $stmt->fetchAll();
+$my_cates = [];
 
+foreach($rows as $k=>$v){
+    if($v['parent_sid']=='0'){
+        $my_cates[] = $v;
+    }
+}
 
+foreach($my_cates as $k1=>$v1){
+    foreach($rows as $k2=>$v2){
+        if($v2['parent_sid']==$v1['sid']){
+            $my_cates[$k1]['children'][] = $v2;
+        }
+    }
+}
 
-echo json_encode($stmt->fetchAll(), JSON_UNESCAPED_UNICODE);
+echo json_encode($my_cates, JSON_UNESCAPED_UNICODE);
 
 
 
