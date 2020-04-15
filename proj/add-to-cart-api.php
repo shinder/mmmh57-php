4217f09213507d4b3ccbd17ab8ba625850712017
shinder <shinder.lin@gmail.com>
2020-04-15 12:00:12 +0800

    購物車功能

---
+++ proj/add-to-cart-api.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * 新增, 修改, 移除
+ */
+
+session_start();
+
+if(! isset($_SESSION['cart'])){
+    $_SESSION['cart'] = [];
+}
+
+$sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
+$qty = isset($_GET['qty']) ? intval($_GET['qty']) : 0;
+
+if(! empty($sid)){
+    // TODO: 判斷有沒有那個商品
+
+    if(! empty($qty)){
+        // 新增或修改
+        $_SESSION['cart'][$sid] = $qty;
+    } else {
+        // 移除
+        unset($_SESSION['cart'][$sid]);
+    }
+}
+
+echo json_encode($_SESSION['cart']);
+
+
+
